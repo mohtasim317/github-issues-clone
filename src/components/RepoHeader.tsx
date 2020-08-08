b65bf2119df7201b03637928464fbb90379d343a
Mohtasim Chowdhury <mohtasim317@hotmail.com>
2020-08-08 18:50:30 -0400

    Added some structure to how RepoHeader Component is laid out.

--- src/components/RepoHeader.tsx
+++ src/components/RepoHeader.tsx
@@ -6,10 +6,14 @@ const RepoHeader: FC = () => {
   return (
     <div className="repo-header">
       <div className="link-and-buttons">
-        <a href="https://www.google.com">facebook/react</a>
-        <div>
-          <div></div>
-          <a></a>
+        <div className="links">
+          <a href="https://www.google.com">facebook/react</a>
+          <a href="https://www.google.com">facebook/react</a>
+        </div>
+        <div className="button-list">
+          <div className="watch button">1</div>
+          <div className="star button">2</div>
+          <div className="fork button">3</div>
         </div>
       </div>
       <div className="tabs">
