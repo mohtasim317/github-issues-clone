6be5914185de87c485fd54e120b3dc588853766f
Mohtasim Chowdhury <mohtasim317@hotmail.com>
2020-08-10 16:29:01 -0400

    Added testing to each component

--- src/__tests__/Issues.test.tsx
+++ src/__tests__/Issues.test.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import Issues from "../components/Issues";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 describe("Issues Component", () => {
   it("renders an issue component with Issue title and details of author", () => {
@@ -11,5 +15,10 @@ describe("Issues Component", () => {
       .toJSON();
     expect(component).toMatchSnapshot();
   });
+  it("contains text", () => {
+    const component = shallow(
+      <Issues key={1} login={"H"} title={"e"} number={3} created_at={"s"} />
+    );
+    expect(component.find("p")).toBeTruthy();
+  });
 });
-
--- src/__tests__/RepoHeader.test.tsx
+++ src/__tests__/RepoHeader.test.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import RepoHeader from "../components/RepoHeader";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 describe("RepoHeader Component Snapshot", () => {
   it("renders the header of the repository including statistics relating to the popularity of the repo", () => {
     const component = renderer.create(<RepoHeader />).toJSON();
     expect(component).toMatchSnapshot();
+  });
+  it("contains links", () => {
+    const component = shallow(<RepoHeader />);
+    expect(component.find("a")).toBeTruthy();
   });
 });
