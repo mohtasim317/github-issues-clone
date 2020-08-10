1f62b8264d5543e3fc22e9f7f1e7e931425a8e04
Mohtasim Chowdhury <mohtasim317@hotmail.com>
2020-08-09 21:13:49 -0400

    CSS finished for RepoHeader component

--- src/components/ContributeMessage.tsx
+++ src/components/ContributeMessage.tsx
@@ -3,12 +3,16 @@ import React, { FC } from "react";
 const ContributeMessage: FC = () => {
   return (
     <div className="contribute-message">
-      <h4 className="contribute">
-        <span role="img" aria-label="hand waving emoji">
-          👋
-        </span>
-        Want to contribute to facebook/react?
-      </h4>
+      <div className="contribute-first-line">
+        <h4 className="contribute">
+          <span role="img" aria-label="hand waving emoji" className="hand-emoji">
+            👋
+          </span>
+          <div className="contribute-first-line-text">
+            Want to contribute to facebook/react?
+          </div>
+        </h4>
+      </div>
       <p className="contribute">
         If you have a bug or an idea, read the contributing guidelines before
         opening an issue.
